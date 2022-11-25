e799bda7621d9e61d476d19f3ef2a26db4f54436
Connor Clark <connordeclark@gmail.com>
2022-11-25 14:22:26 -0600

    Almost have push to new array solved

--- assignment/scripts/test.js
+++ assignment/scripts/test.js
@@ -18,14 +18,15 @@ function addToCollection( album, title, artist, yearPublished ) {
 console.log(addToCollection( 1, `The Blood Inside`, `Ulver`, `2007`));
 console.log(collection.Record.artist);
 */
-record = {};
+
 
 function addToCollection( title, artist, released ){//changed yearPublished to released for shorthand.
+    let record = {};
         record.title = title;
         record.artist = artist;
         record.released = released; 
         collection.push(record);
-        return record;
+        return console.log( record );
 }
 
 console.log(addToCollection( `The Blood Inside`, `Ulver`, `2007`));
@@ -38,13 +39,25 @@ console.log(addToCollection( `Roads to the North`, `Panopticon`, `2014`));
 console.log(collection);
 
 function showCollection( array ) {
-    for( i in array )
-    console.log( array.length);
-    console.log( array [i] );
-    return;
+    for (let i = 0; i < array.length; i++) {
+        console.log( `${array[i].title} by ${array[i].artist} was published in ${array[i].released}.`)
+    }
+    return array.length;
 }
-console.log( showcollection( collection ));
+console.log(showCollection(collection));
 
+let list = [];
 
+function findByArtist( artist ) { //Pushes at least one of the objects into the 
+    for (let i = 0; i < collection.length; i++){
+        console.log( `in for loop:`, collection [i]);
+        if( collection[i].artist === artist ){
+            console.log( `match`);
+            list.push( collection[i] );
+        }
+        return console.log(list);
 
-
+    }
+}
+console.log( findByArtist( `Ulver` ));
+console.log( list );
